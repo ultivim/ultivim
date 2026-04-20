1a5f032ee9c5cb405abbe6f55dd131e1f7679e5d
UmutKzl <umut.exe@icloud.com>
2026-04-20 19:37:09 +0300

    Add blink.cmp

---
+++ lua/ultivim/plugins/cmp.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+
+    version = "1.*",
+
+    ---@module "blink.cmp"
+    ---@type blink.cmp.Config
+    opts = {
+      -- "default" (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- "super-tab" for mappings similar to vscode (tab to accept)
+      -- "enter" for enter to accept
+      -- "none" for no mappings
+      keymap = { preset = "enter" },
+
+      appearance = {
+        nerd_font_variant = "normal",
+      },
+
+      completion = { documentation = { auto_show = false } },
+
+      sources = {
+        default = { "snippets", "path", "lsp", "buffer" },
+      },
+
+      fuzzy = { implementation = "prefer_rust" },
+    },
+    opts_extend = { "sources.default" },
+  },
+}
