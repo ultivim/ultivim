5376110666b3f3a5d89cba81cc6d5f27b56bd53f
UmutKzl <umut.exe@icloud.com>
2026-04-20 19:40:59 +0300

    Add format on save

--- lua/ultivim/plugins/conform.lua
+++ lua/ultivim/plugins/conform.lua
@@ -8,6 +8,10 @@ return {
         rust = { "rustfmt", lsp_format = "fallback" },
         javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
     },
   },
 }
