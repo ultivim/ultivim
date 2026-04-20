f29e3e07136cba61b8839d940a83d06cb74d872b
UmutKzl <umut.exe@icloud.com>
2026-04-20 19:32:25 +0300

    add folds

--- lua/ultivim/opts.lua
+++ lua/ultivim/opts.lua
@@ -12,3 +12,7 @@ vim.opt.colorcolumn = "120" -- add a column in 120. character
 vim.opt.updatetime = 300 -- for CursorHold
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()" -- enable treesitter folding
+vim.wo[0][0].foldmethod = "expr"
+vim.opt.foldlevelstart = 99
