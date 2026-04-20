b1fd8b04ce6d065ae00e0c5efa0b6db97d3da19e
UmutKzl <umut.exe@icloud.com>
2026-04-20 18:44:14 +0300

    Add some options

--- lua/ultivim/opts.lua
+++ lua/ultivim/opts.lua
@@ -1,2 +1,12 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.number = true         -- enable numbers
+vim.opt.relativenumber = true -- enable relativenumbers, try and see
+vim.opt.cursorline = true     -- enable cursorline, highlight current line
+
+vim.opt.tabstop = 2      -- number of spaces will tab button make
+vim.opt.shiftwidth = 2   -- what about autoindent
+vim.opt.expandtab = true -- use spaces, not tabs
+vim.opt.softtabstop = 2  -- number of spaces that a tab counts for while performing editing operations
+
+vim.opt.colorcolumn = "120"
+
+vim.opt.updatetime = 300 -- for CursorHold
