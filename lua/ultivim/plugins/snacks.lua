6c4ecb0009c3ac80ba03ceab03bac68dbc79ef79
UmutKzl <umut.exe@icloud.com>
2026-04-20 21:54:23 +0300

    chore: remove unnecessary things and add indent in snacks.nvim

--- lua/ultivim/plugins/snacks.lua
+++ lua/ultivim/plugins/snacks.lua
@@ -5,16 +5,11 @@ return {
     lazy = false,
     ---@type snacks.Config
     opts = {
-      -- bigfile = { enabled = true },
       dashboard = { enabled = true },
-      explorer = { enabled = true },
-      -- indent = { enabled = true },
-      -- input = { enabled = true },
       picker = { enabled = true },
-      notifier = { enabled = true },
       quickfile = { enabled = true },
+      indent = { enabled = true },
       scope = { enabled = true },
-      -- scroll = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
     },
