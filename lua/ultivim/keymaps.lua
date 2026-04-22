54b402bb2ce721b49cae4bbb9950e5e8db061d0e
UmutKzl <umut.exe@icloud.com>
2026-04-22 17:53:57 +0300

    feat: add diagnostics keymap

--- lua/ultivim/keymaps.lua
+++ lua/ultivim/keymaps.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "<leader>bb", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bv", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+
+vim.keymap.set("n", "gl", function()
+  vim.diagnostic.open_float()
+end, { desc = "Diagnostics - Open Float" })
