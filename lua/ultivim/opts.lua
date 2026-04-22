f62a5b44c0e6b714ac684388d0dc32afb3f76e89
UmutKzl <umut.exe@icloud.com>
2026-04-22 23:27:49 +0300

    feat: enable starting with blank buffer

--- lua/ultivim/opts.lua
+++ lua/ultivim/opts.lua
@@ -16,3 +16,5 @@ vim.opt.clipboard = "unnamedplus"
 vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()" -- enable treesitter folding
 vim.wo[0][0].foldmethod = "expr"
 vim.opt.foldlevelstart = 99
+
+vim.opt.shortmess:append("I") -- start with blank buffer
