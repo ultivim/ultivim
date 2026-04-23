7b304cbd46574b923efdebb65b53ed6a2aec987e
UmutKzl <umut.exe@icloud.com>
2026-04-23 08:35:14 +0300

    feat: add laziness to mason

--- lua/ultivim/plugins/mason-lspconfig.lua
+++ lua/ultivim/plugins/mason-lspconfig.lua
@@ -1,11 +1,17 @@
 return {
   {
     "mason-org/mason-lspconfig.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     opts = {
       ensure_installed = { "lua_ls" },
     },
     dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
+      {
+        "mason-org/mason.nvim",
+        opts = {},
+        cmd = "Mason",
+        build = ":MasonUpdate",
+      },
       "neovim/nvim-lspconfig",
     },
   },
